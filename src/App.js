10d260f86b0862d4b4f25e2883bd58a62d551aec
Abdulkerim Hıcıp <abdulkerimhcp@gmail.com>
2021-08-30 22:24:24 +0300

    searchbox created

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ class App extends React.Component {
     };
   }
 
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
+
   componentDidMount() {
     fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
@@ -28,9 +32,7 @@ class App extends React.Component {
       <div className="App">
         <SearchBox
           placeholder="Search Monsters"
-          handleChange={(e) => {
-            this.setState({ searchField: e.target.value });
-          }}
+          handleChange={this.handleChange}
         />
         <CardList monsters={filteredMonsters} />
       </div>
